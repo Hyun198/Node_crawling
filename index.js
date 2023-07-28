8ce97f997f6e9947687ec3ab402af3add4317486
Hyun <hwn123h@gmail.com>
2023-07-28 13:00:39 +0900

    javascript version

---
+++ index.js
@@ -0,0 +1,21 @@
+/* document.getElementById("myButton").onclick = function () {
+    let username;
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById('myLabel').innerHTML = "hello " + username;
+} */
+
+
+//이건 1을 추가 함
+//age += 1;
+//숫자로
+//age = Number(age);
+
+let x;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+x = Math.PI;
+
+console.log(x);
