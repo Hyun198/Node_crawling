6c0084305e54505ec799958c3c209be2029dd9ab
Hyun <hwn123h@gmail.com>
2023-07-29 13:56:20 +0900

    javascript update

--- index.js
+++ index.js
@@ -11,11 +11,44 @@
 //숫자로
 //age = Number(age);
 
-let x;
-let y = 5;
-let z = 9;
-let maximum;
-let minimum;
-x = Math.PI;
 
-console.log(x);
+//processing
+/* document.getElementById("myButton").onclick = function () { 
+    Time=document.getElementById("myTime").value;
+    if (Time == "open")
+        
+        console.log(Time);
+    else
+        console.log("other");
+}
+ */
+
+document.getElementById("myButton").onclick = function() {
+    Part = document.getElementById('type').value;
+    Time = document.getElementById('time').value;
+    //23:10
+    let hour;
+    let Minute;
+    hour = Time.slice(0,2);
+    minu = Time.slice(3);
+    hour = Number(hour);
+    minu = Number(minu);
+
+    if (Part == '오픈') {
+        if (minu < 30) {
+            hour -= 1;
+            minu -= 30;
+        } else {
+            minu -= 30;
+        }
+        
+        if (minu < 0) {
+            minu = Math.abs(minu)
+        }
+    }
+    console.log("출근시간: "+hour+"시"+minu+"분");
+
+
+}
+
+
