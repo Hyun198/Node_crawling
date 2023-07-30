a9637be82e2a61b93c0f4ee62ec8a4fbdac16ef7
Hyun <hwn123h@gmail.com>
2023-07-30 21:22:23 +0900

    make time input

--- index.js
+++ index.js
@@ -20,6 +20,8 @@
 document.getElementById("myButton").onclick = function() {
     Part = document.getElementById('type').value;
     Time = document.getElementById('time').value;
+    Type = document.getElementById('time_part').value;
+    
     //23:10
     let hour;
     let minu;
@@ -29,12 +31,16 @@ document.getElementById("myButton").onclick = function() {
     let end_min;
     hour = Time.slice(0,2);
     minu = Time.slice(3);
+
+    Type_hour = Type.slice(0,1);
+    Type_minu = Type.slice(2);
+/*     console.log(Type_hour);
+    console.log(Type_minu); */
+    Type_hour = Number(Type_hour);
+    Type_minu = Number(Type_minu);
     //형 변환
     hour = Number(hour);
     minu = Number(minu);
-
-    console.log(hour);
-    console.log(minu);
 
     if (Part == '오픈') {
         if (minu < 30) {
@@ -44,8 +50,8 @@ document.getElementById("myButton").onclick = function() {
             start_hour = hour;
             start_min = minu - 30;
         }
-        end_hour = start_hour + 7;
-        end_min = start_min + 30;
+        end_hour = start_hour + Type_hour;
+        end_min = start_min + Type_minu;
         if (start_min < 0) {
             start_min = Math.abs(start_min);
         } 
@@ -74,7 +80,7 @@ document.getElementById("myButton").onclick = function() {
 
         if (minu < 30) {
             start_min = minu + 30;
-            start_hour = hour - 8;
+            start_hour = hour - Type_hour -1;
 
             if (start_min == 0) {
                 document.getElementById('myLabel').innerHTML = "출근시간 : " + start_hour + "시" + "00분";
@@ -82,8 +88,8 @@ document.getElementById("myButton").onclick = function() {
                 document.getElementById('myLabel').innerHTML = "출근 시간 : " + start_hour + "시" + start_min + "분";
             }
         } else {
-            start_min = minu - 30;
-            start_hour = hour - 7;
+            start_min = minu - Type_minu;
+            start_hour = hour - Type_hour;
 
             if (start_min == 0) {
                 document.getElementById('myLabel').innerHTML = "출근시간 : " + start_hour + "시" + "00분";
@@ -95,7 +101,7 @@ document.getElementById("myButton").onclick = function() {
         
         
     } else if (Part == '마감2') {
-        start_hour = hour - 7;
+        start_hour = hour - Type_hour;
         start_min = minu;
         document.getElementById('myLabel').innerHTML = "출근 시간 : " +start_hour + "시" + start_min + "분";
     }
