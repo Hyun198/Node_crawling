ff76b6eb0a106e8d31f6be587624a8ae84e48670
Hyun <hwn123h@naver.com>
2023-08-16 22:40:10 +0900

    crawling update using axios

--- index.js
+++ index.js
@@ -3,32 +3,100 @@ const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const url = '';
 
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res) {
-    res.render('cgv')
-});
-
-
-app.get('/get-movie-times', async(req, res) => {
-    try {
-        const response = await axios.get('http://www.cgv.co.kr/theaters/?areacode=02&theaterCode=0298&date=20230814');
-        const html = response.data;
-        const $ = cheerio.load(html);
-
-        const movieTimes = [];
-        $('col-times').each((index, element) => {
-            const title = $(element).text().trim();
-            movieTimes.push(title);
+axios(url).then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const $productList = $(".space");
+    let products = []
+    $productList.each((idx, node) => {
+        products.push({
+            title: $(node).find('.txt').text(),
+            img: $(node).find('.thumbnail > a > img').attr('src')
         })
+        
+    });
+    console.log(products);
+})
 
-        res.json(movieTimes);
-    } catch (error) {
-        console.error('영화 시간 크롤링 오류:', error);
-        res.status(500).send('영화 시간 크롤링 중 오류가 발생했습니다.');
+
+
+
+
+
+
+
+
+
+
+/* const c = new Crawler({
+    maxConnections: 10,
+    // This will be called for each crawled page
+    callback: (error, res, done) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($('title').text());
+            const $bodyList = $('div.sect-showtimes').children('div.info-movie');
+            let movieList = [];
+            $bodyList.each(function (i, elem) {
+                movieList[i] = $(this).find('strong').text();
+                console.log(i);
+            })
+            console.log(movieList);
+        }
+        done();
     }
 });
 
+c.queue('http://www.cgv.co.kr/theaters/?areacode=02&theaterCode=0298&date=20230816'); */
 
-app.listen(3000)
+
+
+
+
+
+
+
+
+
+/* app.set('view engine', 'ejs'); */
+
+/* 
+async function getHtml () {
+    try {
+        const response = await axios.get('http://www.cgv.co.kr/theaters/?areacode=02&theaterCode=0298&date=20230816')
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+(async () => {
+    try {
+        const html = await getHtml();
+        const ulList = [];
+        const $ = cheerio.load(html);
+        const $bodyList = $("div.info-timetable ul").children("li");
+        
+        $bodyList.each(function (i, elem) {
+            ulList[i] = {
+                screen: $(this).find('a').attr('data-screenkorname'),
+                time: $(this).find('a').attr('data-playstarttime')
+            };
+        });
+        
+        console.log(ulList);
+    } catch (error) {
+        console.error('에러 발생:', error);
+    }
+})();
+    
+
+    
+ */
+
